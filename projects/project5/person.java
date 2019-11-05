673e8989517ab0e51b9354ff9573785e49cbad22
Rifa Safeer Shah <rifashah28@gmail.com>
2019-11-05 11:34:34 -0800

    Create person.java

---
+++ projects/project5/person.java
@@ -0,0 +1,20 @@
+
+//Name: Rifa Safeer Shah
+//Assignment: Queue Project
+
+/**
+ * The person.java program is used to model the distribution of iPhones
+ * to the customers in the queue using their first name and last name
+ * INPUTS: First Name, Last Name, Size of the Queue
+ * OUTPUT: A queue of people to receive the iPhone
+ */
+
+public class person {
+	String FirstName;
+	String LastName;
+	
+	public person(String fn, String ln) {
+		FirstName = fn;
+		LastName = ln;
+	}
+}
